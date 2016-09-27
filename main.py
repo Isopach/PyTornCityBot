3531ef060e69d3d1f0da49e0358e1ae40394a65f
Isopach <Isopach@users.noreply.github.com>
2016-09-27 12:09:18 +0900

    Edit comments

--- main.py
+++ main.py
@@ -8,11 +8,11 @@ import random
 # Create a new instance of the Chrome driver
 driver = webdriver.Chrome('C:\chromedriver\chromedriver.exe')
 
-# go to the google home page
+# go to the torn city home page
 driver.get("http://www.torn.com")
 
-#Login1
-time.sleep(random.uniform(3,5))
+#Login
+time.sleep(random.uniform(1,3))
 
 username1 = driver.find_element_by_id("player")
 username1.send_keys("EMAIL")
@@ -20,12 +20,12 @@ username1.send_keys("EMAIL")
 password1 = driver.find_element_by_id("password")
 password1.send_keys("PASSWORD")
 password1.submit()
-time.sleep(random.uniform(3,5))
+time.sleep(random.uniform(1,3))
 
-#Deposit money1
+#Deposit money
 driver.get("http://www.torn.com/properties.php#/p=options&tab=vault")
 #vault = driver.find_element_by_id("icon32").click()
-time.sleep(random.uniform(3,5))
+time.sleep(random.uniform(1,3))
 
 #Scroll down to bottom of page
 driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
@@ -33,15 +33,15 @@ driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
 vaultsubmit1 = driver.find_element_by_xpath('//*[@id="properties-page-wrap"]/div[3]/div[8]/div[3]/form[2]/div[2]/span/span/input')
 vaultsubmit1.click()
 
-time.sleep(random.uniform(3,5))
+time.sleep(random.uniform(1,3))
 #Do Gym
 """ gym1 = driver.find_element_by_xpath('//*[@id="nav-gym"]/a').click()
 recaptchaA = driver.find_element_by_xpath('//*[@id="ui-id-2"]/span').click()
 recaptchaB = driver.find_element_by_xpath('//*[@id="recaptcha-anchor"]/div[5]').click()
 """
 
-#Logout1
+#Logout
 logout1 = driver.find_element_by_class_name("logout").click()
-time.sleep(random.uniform(3,5))
+time.sleep(random.uniform(1,3))
 
 driver.quit()
